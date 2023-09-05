4992ccda3ebe7ba92809f77bf1259941f845310b
Amir K <amir@kamali.io>
2023-09-05 01:43:30 -0700

    Initial Commit

---
+++ GaugeView/GaugeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeViewApp.swift
+//  GaugeView
+//
+//  Created by Amir on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GaugeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
